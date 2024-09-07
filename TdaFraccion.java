1dd454af72babdc29941d9d9e036417faf9507c4
calepi365 <rcaleb090@gmail.com>
2024-09-07 09:27:28 -0400

    tda fraccion

---
+++ TdaFraccion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TdaFraccion {
+
+    public static void main(String[] args) {
+
+        int numerador;
+        int denominador;
+        int resta;
+        int multiplicacion;
+        int division;
+    System.out.println("ingrese un numero : ");
+      
+
+    }
+}
